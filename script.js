7f4143a790ba01550c19895cce27e52971508987
unknown <cervinkova.sonik@gmail.com>
2024-02-03 18:40:18 +0000

    function to display 5-day weatherforecast added

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function fetchForecastToday (coordUrlSection){
 var apiCall = "http://api.openweathermap.org/data/2.5/forecast?" + coordUrlSection + "&appid=" + apiKey;
 var currentDate = dayjs().format("DD/MM/YYYY");
 
-console.log(apiCall);
+// console.log(apiCall);
 
 fetch (apiCall)
 .then(function (response){
@@ -31,6 +31,7 @@ fetch (apiCall)
     // display city, date, icon in today section
     document.getElementById("today").style.border = "1px solid black";
     document.getElementById("today").style.padding = "10px";
+    document.getElementById("today").style.borderRadius = "5px";
     var headerToday = document.createElement("h2");
     var iconImg = document.createElement("img");
     iconImg.setAttribute("src", iconUrl);
@@ -58,45 +59,84 @@ fetch (apiCall)
     todaySection.appendChild(humidityEl);
 
     // console logs
-    console.log(data);
-    console.log(cityName);
-    console.log(date);
-    console.log(iconId);
-    console.log(iconUrl);
-    console.log(headerToday);
-    console.log(temp);
-    console.log(wind);
-    console.log(humidity);
+    // console.log(data);
+    // console.log(cityName);
+    // console.log(date);
+    // console.log(iconId);
+    // console.log(iconUrl);
+    // console.log(headerToday);
+    // console.log(temp);
+    // console.log(wind);
+    // console.log(humidity);
 
-    for (var i = 0; i < data.list.length; i++){
-        var dateHour = data.list[i].dt_txt;
-        var midday = dateHour.includes("12");
-        if (midday){
-            console.log(dateHour);
-        }
-    }
+    // for (var i = 0; i < data.list.length; i++){
+    //     var dateHour = data.list[i].dt_txt;
+    //     var midday = dateHour.includes("12");
+    //     if (midday){
+    //         console.log(dateHour);
+    //     }
+    // }
   
 });
 }
-
 // function to display 5 day forecast
-// function fetchForecastFiveDays (coordUrlSection){
-//     var apiCall = "http://api.openweathermap.org/data/2.5/forecast?" + coordUrlSection + "&appid=" + apiKey;
-//     fetch (apiCall)
-//     .then(function (response){
-//         return response.json();
-//     })
-//     .then(function (data){
-//         var todaySection = document.getElementById("today");
-//         // empty today section first
-//         todaySection.innerHTML = "";
-//         for (var i = 0; i < data.list; i++){
-//             var dateHour = data.list[i].dt_txt;
-//             console.log(dateHour);
-//         }
+function fetchForecastFiveDays (coordUrlSection){
+    var apiCall = "http://api.openweathermap.org/data/2.5/forecast?" + coordUrlSection + "&appid=" + apiKey;
+    fetch (apiCall)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data){
+        var forecastSection = document.getElementById("forecast");
+        // empty today section first
+        forecastSection.innerHTML = "";
+        for (var i = 0; i < data.list.length; i++){
+            var dateHour = data.list[i].dt_txt;
+            var midday = dateHour.includes("12");
+            // retrieving weather icon ID + build icon URL
+            var iconId = data.list[i].weather[0].icon;
+            var iconUrl =  "https://openweathermap.org/img/wn/" + iconId + ".png"
+            if (midday){
+                var year = dateHour.slice(0,4);
+                var month = dateHour.slice(5,7);
+                var day = dateHour.slice(8,10);
+                var dateObj = new Date(year, month, day);
+                var date = dayjs(dateObj).format("DD/MM/YYYY");
+                var colDiv = document.createElement("div");
+                colDiv.setAttribute("class", "col");
+                forecastSection.appendChild(colDiv);
+                var cardDiv = document.createElement("div");
+                cardDiv.setAttribute("class", "card");
+                colDiv.appendChild(cardDiv);
+                var cardBody = document.createElement("div");
+                cardBody.setAttribute("class", "card-body");
+                cardDiv.append(cardBody);
+                var cardTitle = document.createElement("h6");
+                cardTitle.setAttribute("class", "card-title");
+                cardTitle.textContent = date;
+                cardBody.appendChild(cardTitle);
+                var iconImg = document.createElement("img");
+                iconImg.setAttribute("src", iconUrl);
+                iconImg.setAttribute("alt", "weather-icon");
+                cardBody.appendChild(iconImg)
+                var temp = (data.list[i].main.temp - 273.15).toFixed(2);
+                var tempEl = document.createElement("p");
+                tempEl.textContent = `Temp: ${temp}°C`;
+                cardBody.appendChild(tempEl);
+                var wind = data.list[i].wind.speed;
+                var windEl = document.createElement("p");
+                windEl.textContent = `Wind: ${wind} KPH`;
+                cardBody.appendChild(windEl);
+                var humidity = data.list[i].main.humidity;
+                var humidityEl = document.createElement("p");
+                humidityEl.textContent = `Humidity: ${humidity}%`;
+                cardBody.appendChild(humidityEl);
+            }
+        }
 
-// });
-// }
+});
+}
+
 
 // Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name? 
 // Geocoding API
@@ -111,18 +151,18 @@ function fetchCoord(cityInput) {
         // console.log(data)
         // retrieving city name
         var cityNameGeo = data[0].name;
-        console.log(cityNameGeo);
+        // console.log(cityNameGeo);
 
         // retrieving coord
         var cityLat = data[0].lat;
         var cityLon = data[0].lon;
         var coordUrlSection = "lat=" + cityLat + "&lon=" + cityLon
-        console.log(cityLat);
-        console.log(cityLon);
-        console.log(coordUrlSection);
+        // console.log(cityLat);
+        // console.log(cityLon);
+        // console.log(coordUrlSection);
 
         fetchForecastToday(coordUrlSection);
-        // fetchForecastFiveDays (coordUrlSection);
+        fetchForecastFiveDays (coordUrlSection);
     }); 
 }
 
